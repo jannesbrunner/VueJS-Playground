7d4e319de4b14d07fc9eb69006f550c9192a8e96
Jannes Brunner <jannes.brunner@student.htw-berlin.de>
2018-08-17 10:24:58 +0100

    Add some more v specific data binding

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ const helloWorld = new Vue({
         message: "This is my first Vue Template"
     }
 });
+
+const example = new Vue({
+    el: '#example',
+    data: {
+        title: "Hello!",
+        message: "This is a message for you",
+        name: "User",
+        img: {
+            src: 'https://placeimg.com/200/200/animals',
+            alt: 'A placeholder image'
+        }
+    }
+});
